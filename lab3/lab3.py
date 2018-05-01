cc7e8d24afd2a4999208731e6363dfbfabdd22a9
Ponts <pbrink@kth.se>
2018-05-01 19:12:37 +0200

    Yeeehaaa workin

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -4,6 +4,7 @@ import matplotlib.image as mpimg
 import random
 import pandas as pd
 import glob
+import copy
 filepath = "cifar-10-batches-py/"
 
 def unpickle(file):
@@ -66,7 +67,7 @@ class Network():
 			self.vav.append(   np.zeros((setup[l],1) )) 
 		self.alpha = alpha
 
-	def forwardPass(self, X, first = False):
+	def forwardPass(self, X):
 		S = []
 		S_ = []
 		h = []
@@ -75,40 +76,56 @@ class Network():
 		h.append(X)
 		for l in range(len(self.W)-1):
 			S.append((np.dot(self.W[l],h[l]) + self.b[l]))
-			mul, vl = self.calculateNormalize(S[l],l)
-			mu.append(mul)
-			v.append(vl)
-			S_.append(self.BatchNormalize(S[l],mul,vl,l))
 			if self.useBatch:
+				mul, vl = self.calculateNormalize(S[l])
+				mu.append(mul)
+				v.append(vl)
+				S_.append(self.BatchNormalize(S[l],mul,vl))
 				s = S_[l]
-				if first:
-					self.muav[l] = mul
-					self.vav[l] = vl
-				else:
-					self.muav[l] = self.alpha * self.muav[l] + (1.-self.alpha)*mul
-					self.vav[l] = self.alpha * self.vav[l] + (1.-self.alpha)*vl
+				
 			else:
 				s = S[l]
 			if self.activationFunc == 'RELU':
 				h.append(self.relu(s))
+			elif self.activationFunc == 'SIGM':
+				h.append(self.sigmoid(s))
 
 		S.append(np.dot(self.W[-1],h[-1]) + self.b[-1])
 		return S, S_, mu, v, h, self.getP(S[-1])
+
+	def testPass(self, X):
+		S = []
+		S_ = []
+		h = []
+		h.append(X)
+		for l in range(len(self.W)-1):
+			S.append((np.dot(self.W[l],h[l]) + self.b[l]))
+			if self.useBatch:
+				S_.append(self.BatchNormalize(S[l],self.muav[l],self.vav[l]))
+				s = S_[l]
+			else:
+				s = S[l]
+			if self.activationFunc == 'RELU':
+				h.append(self.relu(s))
+			elif self.activationFunc == 'SIGM':
+				h.append(self.sigmoid(s))
+		S.append(np.dot(self.W[-1],h[-1]) + self.b[-1])
+		return self.getP(S[-1])
 
 	def evaluateClassifier(self, X):
 		S, S_, mu, v, h, P = self.forwardPass(X)
 		return P
 
 	def computeAccuracy(self, X, Y):
-		P = self.evaluateClassifier(X)
+		P = self.testPass(X)
 		acc = 0.0
 		for sample in range(len(Y.T)):
 			if np.argmax(P[:,sample]) == np.argmax(Y[:,sample]):
-				acc+=1		
+				acc+=1
 		return (acc/Y.shape[1])*100
 
 	def computeCost(self,X, Y):
-		P = self.evaluateClassifier(X)
+		P = self.testPass(X)
 		L = self.getL(P,Y)
 		L /= X.shape[1]
 		J = self.getJ(L)
@@ -124,17 +141,16 @@ class Network():
 	def relu(self, h):
 		return  np.where(h>0,h,0)
 
-	def calculateNormalize(self, s, l):
+	def calculateNormalize(self, s):
 		mu = np.mean(s,axis=1).reshape(-1,1)
-		v = np.var(s,axis=1).reshape(-1,1)
+		v = np.mean((s - mu)**2, axis=1).reshape(-1,1)
 		return mu, v
 		
-
-
-	def BatchNormalize(self, s, mu, v, l):
-		diag = np.diagflat(v + self.e)
-		left = np.power(diag,-0.5)
-		ret = np.dot(left,s - mu)
+	def BatchNormalize(self, s, mu, v):
+		vb = np.power(v + self.e,-0.5)
+		diag = np.diagflat(vb)
+		ret = np.dot(diag,s - mu)
+		
 		return ret
 
 	def getP(self,S2):
@@ -156,13 +172,7 @@ class Network():
 		total = 0.0
 		for W in self.W:
 			total += np.sum(W**2)
-		#for w in np.nditer(self.W1):
-		#	total+= w**2
-		#for w in np.nditer(self.W2):
-		#	total+= w**2
 		return self.regTerm*total
-
-
 
 	def updateWithBatch(self, X, Y, first):
 		djdw = [np.zeros((self.W[i].shape)) for i in range(len(self.W))]
@@ -175,7 +185,7 @@ class Network():
 		for i in range(len(djdw)):
 			djdw[i]/=X.shape[1]
 			djdb[i]/=X.shape[1]
-
+		#print(djdw[0][0][0])
 		for i in range(len(self.WV)):
 			self.WV[i] = self.p * self.WV[i] + self.eta*djdw[i] + 2*self.regTerm*self.W[i]
 			self.bV[i] = self.p * self.bV[i] + self.eta*djdb[i]
@@ -183,50 +193,36 @@ class Network():
 			self.b[i] -= self.bV[i]
 
 	def batchNormBackPass(self, g, S, mu, v):
+		Vb = v + self.e
+		#print("---------")
+		#print(Vb)
+		vBSq = np.power(Vb, -0.5)
+		#print(vBSq)
+		sMu = S - mu
 		n = S.shape[1]
-		Vb = np.diagflat(v + self.e)
-		DS_Vb = np.zeros((S.shape[1], S.shape[0], S.shape[0]))
-		for i in range(n):
-			DS_Vb[i] = np.multiply(-0.5*np.power(Vb,-3./2.),np.diag(S[:,i]-mu)) 
-
-		DJVb = np.zeros((1, S.shape[0]))
-		for i in range(n):
-			DJVb += np.dot(g[i],DS_Vb[i])
-		#for i in range(n):
-		#	DJVb = np.dot(g[i],DS_Vb[:,i])
-		#print(DJVb.shape)
-		powerd = -np.power(Vb,-0.5)
-		DJmu = np.zeros((1, S.shape[0]))
-		for i in range(n):
-			DJmu += np.dot(g[i],powerd)
-
-
-		#DJmu = g * powerd.T   # FEL!
-		#left = np.dot(g,np.power(Vb,-0.5)).T
-		
-		DJS_ = np.zeros((g.shape))
-		for i in range(n):
-			left = np.dot(g[i],np.power(Vb,-0.5))
-			diag = np.diag(S[:,i] - mu)
-			middle = (0.5*n)*(np.dot(DJVb,diag))
-			DJS_[i] = (left + middle + DJmu/n)
-
-		return DJS_
-
-
-
+		gVgSq = np.multiply(g, vBSq)
+		gradVar = -0.5 * np.sum(np.multiply(np.multiply(g,np.power(Vb, -3./2)), sMu), axis=1).reshape(-1,1)
+		gradMu = - np.sum(gVgSq, axis=1).reshape(-1,1)
+		return gVgSq + (2/n) * np.multiply(gradVar, sMu) + gradMu/n
 
 	def calculateGradient(self, x, y, first):
 		djdw = [np.zeros((self.W[i].shape)) for i in range(len(self.W))]
 		djdb = [np.zeros(self.b[i].shape) for i in range(len(self.b))]
-		S, S_, mu, v, h, P = self.forwardPass(x, first)
+		S, S_, mu, v, h, P = self.forwardPass(x)
+		if first and self.useBatch:
+			self.muav = copy.deepcopy(mu)
+			self.vav = copy.deepcopy(v)
+		elif self.useBatch:
+			for l in range(len(self.muav)):
+				self.muav[l] = self.alpha * self.muav[l] + (1.-self.alpha)*mu[l]
+				self.vav[l] = self.alpha * self.vav[l] + (1.-self.alpha)*v[l]
 		#backward
 		g = P - y
 		l = len(self.W)-1
 		while l >= 0:
 			djdb[l] = g
 			djdw[l] = np.dot(g,h[l].T)
-			g = np.dot(g.T,self.W[l])
+			g = np.dot(self.W[l].T,g)
 			
 			if l > 0:
 				if self.useBatch:
@@ -234,16 +230,10 @@ class Network():
 				else:
 					s = S[l-1]
 				if self.activationFunc == 'RELU':
-					ind = np.where(s>0,1,0)
-				#HERE SHOULD BATCH NORM GO
-				for i in range(g.shape[0]):
-					g[i,:] = np.dot(g[i,:],np.diag(ind[:,i]))
-				
+					ind = np.where(s>0.,1.,0.)
+				g = np.multiply(g,ind)
 				if self.useBatch:
 					g= self.batchNormBackPass(g,S[l-1],mu[l-1],v[l-1])
-				g = g.T
-				
-				
 			l-=1
 		
 		for i in range(len(djdw)):
@@ -268,6 +258,7 @@ class Network():
 				jEnd = j * self.batchSize
 				self.updateWithBatch(self.trainX[:,jStart:jEnd], self.trainY[:,jStart:jEnd], first)
 				first = False
+				
 			if decayEta and i % 10 == 0:
 				self.eta = self.eta*0.95
 			print(i)
@@ -292,15 +283,15 @@ if __name__ == "__main__":
 	trainY = np.concatenate((trainY, trainY2[:,0:9000], trainY3, trainY4, trainY5), axis=1)
 	validationX, valLabelY, _, validationY = getData("data_batch_2")
 
-	#mean = getMean(trainX)
-	#trainX = trainX - np.tile(mean,(1,trainX.shape[1]))
-	#validationX = validationX - np.tile(mean, (1, validationX.shape[1]))
-	#testX = testX - np.tile(mean, (1, testX.shape[1]))
+	mean = getMean(trainX)
+	trainX = trainX - np.tile(mean,(1,trainX.shape[1]))
+	validationX = validationX - np.tile(mean, (1, validationX.shape[1]))
+	testX = testX - np.tile(mean, (1, testX.shape[1]))
 	# PRETTY GOOD RELU
-	eta = 0.00025087721185986918
-	lambd = 6.865912979562136e-07
-	network = Network([3072, 50, 10], trainX, trainY, validationX, validationY, eta, regTerm=lambd, useBatch = True)
-	loss, valLoss, trainAcc, validAcc = network.fit(epochs = 5, earlyStopping = False)
+	eta = 0.00001
+	lambd = 0.0000000001
+	network = Network([3072, 500, 250, 10], trainX, trainY, validationX, validationY, eta, regTerm=lambd, useBatch = True)
+	loss, valLoss, trainAcc, validAcc = network.fit(epochs = 50, earlyStopping = True)
 
 	plt.plot(loss, label="train loss")
 	plt.plot(valLoss, label="validation loss")
@@ -313,5 +304,6 @@ if __name__ == "__main__":
 	plt.legend()
 	plt.xlabel("Epochs")
 	plt.ylabel("Accuracy")
+	plt.grid(True)
 	plt.show()
 	print(network.computeAccuracy(testX, testY))
